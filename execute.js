ab5af1e81add649bb5380d5e4c20aaf7f37be2f2
yuhanas yulianto <yulianto.yuhanas@gmail.com>
2021-10-05 20:55:18 +0700

    test scheduler without really hit api

--- execute.js
+++ execute.js
@@ -1,10 +1,31 @@
 const talenta = require("./index");
-const { cookiesTalenta, longitude, latitude } = require("./config");
+const { cookiesTalenta, longitude, latitude, timeClockIn, timeClockOut, onWeekdays } = require("./config");
 
 (async () => {
   if (process.argv[2] == "clockin") {
-    console.log(await talenta.clockIn({ lat: latitude, long: longitude, cookies: cookiesTalenta, desc: 'Hello I am In' }));
+    console.log(
+      await talenta.clockIn({
+        lat: latitude,
+        long: longitude,
+        cookies: cookiesTalenta,
+        desc: "Hello I am In",
+        timeClockIn,
+        onWeekdays,
+      })
+    );
   } else if (process.argv[2] == "clockout") {
-    console.log(await talenta.clockOut({ lat: latitude, long: longitude, cookies: cookiesTalenta, desc: 'Goodbye I am Out' }));
+    console.log(
+      await talenta.clockOut({
+        lat: latitude,
+        long: longitude,
+        cookies: cookiesTalenta,
+        desc: "Goodbye I am Out",
+        timeClockOut,
+        onWeekdays,
+      })
+    );
   }
+  // else if (process.argv[2] == "scheduled") {
+  //   console.log(await talenta.scheduled({ lat: latitude, long: longitude, cookies: cookiesTalenta, desc: "Goodbye I am Out" }));
+  // }
 })();
--- index.js
+++ index.js
@@ -1,49 +1,78 @@
-const axios = require('axios');
-const FormData = require('form-data');
-const codec = require('string-codec');
+const axios = require("axios");
+const FormData = require("form-data");
+const codec = require("string-codec");
+const cron = require("node-cron");
 
 const prepForm = (obj) => {
   const { long, lat, desc, cookies, isCheckOut = false } = obj;
   const data = new FormData();
-  const status = (isCheckOut) ? 'checkout' : 'checkin';
+  const status = isCheckOut ? "checkout" : "checkin";
 
-  const longEncoded = codec.encoder(codec.encoder(long,'base64'),'rot13');
-  const latEncoded = codec.encoder(codec.encoder(lat,'base64'),'rot13');
+  const longEncoded = codec.encoder(codec.encoder(long, "base64"), "rot13");
+  const latEncoded = codec.encoder(codec.encoder(lat, "base64"), "rot13");
 
-  data.append('longitude', longEncoded);
-  data.append('latitude', latEncoded);
-  data.append('status', status);
-  data.append('description', desc);
+  data.append("longitude", longEncoded);
+  data.append("latitude", latEncoded);
+  data.append("status", status);
+  data.append("description", desc);
 
   const config = {
-    method: 'post',
-    url: 'https://hr.talenta.co/api/web/live-attendance/request',
-    headers: { 
-      'Cookie': cookies, 
-      ...data.getHeaders()
+    method: "post",
+    url: "https://hr.talenta.co/api/web/live-attendance/request",
+    headers: {
+      Cookie: cookies,
+      ...data.getHeaders(),
     },
-    data : data
+    data: data,
   };
 
   return config;
 };
 
+const parseTime = (time) => {
+  return time.split(":");
+};
+
 const attendancePost = async (obj) => {
   const config = prepForm(obj);
-  const resp = await axios(config);
+  // const resp = await axios(config);
 
-  return resp.data;
-}
+  // return resp.data;
+  return `POST attendance with ${JSON.stringify(config)}`;
+};
+
+const scheduler = async (time, callback) => {
+  const [hour, min] = parseTime(time);
+
+  const task = cron.schedule(`${min} ${hour} * * 1-5`, async () => {
+    // const task = cron.schedule(`*/5 * * * * *`, async () => {
+    console.log(await callback);
+  });
+
+  return task;
+};
 
 const clockIn = async (obj) => {
-  return await attendancePost({ ...obj, isCheckOut: false});
-}
+  const { timeClockIn } = obj;
+  if (timeClockIn) {
+    const task = await scheduler(timeClockIn, attendancePost({ ...obj, isCheckOut: false }));
+    return `Start scheduler for your clockIn every ${timeClockIn}`;
+  }
+
+  return await attendancePost({ ...obj, isCheckOut: false });
+};
 
 const clockOut = async (obj) => {
-  return await attendancePost({ ...obj, isCheckOut: true});
-}
+  const { timeClockOut } = obj;
+  if (timeClockOut) {
+    const task = await scheduler(timeClockOut, attendancePost({ ...obj, isCheckOut: true }));
+    return `Start scheduler for your clockOut every ${timeClockOut}`;
+  }
+
+  return await attendancePost({ ...obj, isCheckOut: true });
+};
 
 module.exports = {
   clockIn,
-  clockOut
-}
+  clockOut,
+};
