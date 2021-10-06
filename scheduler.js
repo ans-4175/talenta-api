c2865b652257a3bdcfd9b97ead63942fe957f010
Ahmad Anshorimuslim Syuhada <ans4175@gmail.com>
2021-10-06 19:09:44 +0700

    fix(scheduler): add process exit

--- scheduler.js
+++ scheduler.js
@@ -1,6 +1,6 @@
 const talenta = require("./index");
 const cron = require("node-cron");
-const pm2 = require("pm2");
+
 const { cookiesTalenta, longitude, latitude, timeClockIn, timeClockOut } = require("./config");
 
 const parseTime = (time) => {
@@ -22,5 +22,8 @@ const scheduler = async (time, callback) => {
     console.log(`Start scheduler for your clockIn every ${timeClockIn} and clockOut every ${timeClockOut}`);
     await scheduler(timeClockIn, () => talenta.clockIn({ lat: latitude, long: longitude, cookies: cookiesTalenta, desc: "Hello I am In" }));
     await scheduler(timeClockOut, () => talenta.clockOut({ lat: latitude, long: longitude, cookies: cookiesTalenta, desc: "Goodbye I am Out" }));
+  } else {
+    console.error("✖︎ Error: timeClockIn and timeClockOut undefined");
+    process.exit(1);
   }
 })();
